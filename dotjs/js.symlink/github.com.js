52f2718e0f639b060a7113fce9277334141dd56c
Caleb Thompson <caleb@solsystemscompany.com>
2012-05-31 17:15:26 -0400

    Comment github.com.js

--- dotjs/js.symlink/github.com.js
+++ dotjs/js.symlink/github.com.js
@@ -1,3 +1,7 @@
+// I'm really not a fan of the colored backgrounds for mini-icons in the
+// newsfeed.
+// I make them go away and make the mini-icons in the newsfeed match the rest of
+// the mini-icons
 $('.news .mini-icon')
   .css('color', '#666')
   .css('background', 'none')
