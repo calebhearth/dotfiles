a9c28b6d9927b5e599047cfd757f9b3a940d1194
Caleb Hearth <caleb@calebhearth.com>
2025-03-07 11:09:52 -0700

    Appease new neovim warning

--- vim/.config/nvim/plugin/lsp.lua
+++ vim/.config/nvim/plugin/lsp.lua
@@ -8,23 +8,23 @@ end
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client.supports_method('textDocument/hover') then
+    if client:supports_method('textDocument/hover') then
       -- vim.bo[args.buf].keywordprg = 'v:lua.vim.lsp.buf.hover'
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {
         buffer = args.buf,
         desc = 'vim.lsp.buf.hover()'
       })
     end
-    if client.supports_method('textDocument/definition') then
+    if client:supports_method('textDocument/definition') then
       vim.bo[args.buf].tagfunc = 'v:lua.vim.lsp.tagfunc'
     end
-    if client.supports_method('textDocument/completion') then
+    if client:supports_method('textDocument/completion') then
       vim.bo[args.buf].omnifunc = ':lua vim.lsp.omnifunc'
     end
-    if client.supports_method('textDocument/rangeFormatting') then
+    if client:supports_method('textDocument/rangeFormatting') then
       vim.bo[args.buf].formatexpr = 'v:lua.vim.lsp.formatexpr()'
     end
-    -- if client.supports_method('textDocument/diagnostics') then
+    -- if client:supports_method('textDocument/diagnostics') then
     --   vim.keymap.set("n", "[d", vim.lsp.diagnostic.goto_prev, {
     --     buffer = 0,
     --     desc = 'vim.lsp.diagnostic.goto_prev()'
