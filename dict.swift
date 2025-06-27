222bfc508467d81043c05b999685f5008b87e262
Caleb Hearth <caleb@calebhearth.com>
2025-06-27 09:56:49 -0600

    wip - rewrite dict, jobs

--- dict.swift
+++ dict.swift
@@ -1,43 +1,43 @@
 // This is the source for bin/dict
 // Compile with `swiftc -parse-as-library -o bin/dict`
 
-import Foundation
 import CoreServices.DictionaryServices
+import Foundation
 
 @main
 struct Dict {
-    static func define(_ word: String) -> String? {
-        let nsstring = word as NSString
-            let cfrange = CFRange(location: 0, length: nsstring.length)
+	static func define(_ word: String) -> String? {
+		let nsstring = word as NSString
+		let cfrange = CFRange(location: 0, length: nsstring.length)
 
-            guard let definition = DCSCopyTextDefinition(nil, nsstring, cfrange) else {
-                return nil
-            }
+		guard let definition = DCSCopyTextDefinition(nil, nsstring, cfrange) else {
+			return nil
+		}
 
-        var def = String(definition.takeUnretainedValue())
-            .replacing("▶", with: "\n")
-            .replacing(" •", with: "\n  •")
-            .replacing("ORIGIN", with: "\n\nORIGIN\n  ")
-            .replacing("DERIVATIVES", with: "\n\nDERIVATIVES\n  ")
-            .replacing("PHRASES", with: "\n\nPHRASES\n  ")
+		var def = String(definition.takeUnretainedValue())
+			.replacing("▶", with: "\n")
+			.replacing(" •", with: "\n  •")
+			.replacing("ORIGIN", with: "\n\nORIGIN\n  ")
+			.replacing("DERIVATIVES", with: "\n\nDERIVATIVES\n  ")
+			.replacing("PHRASES", with: "\n\nPHRASES\n  ")
 
-        let entryNumberRegex = try! Regex("[:space:]+(?<entryNumber>[0-9]+)[:space:]+")
-        def.replace(entryNumberRegex, with: { "\n\n\($0.0). " })
-        return def
-    }
+		let entryNumberRegex = try! Regex("[:space:]+(?<entryNumber>[0-9]+)[:space:]+")
+		def.replace(entryNumberRegex, with: { "\n\n\($0.0). " })
+		return def
+	}
 
-    static func main() {
-        let args = CommandLine.arguments
-            if args.count < 2 {
-                print("Usage: dict <word>")
-                    return
-            }
+	static func main() {
+		let args = CommandLine.arguments
+		if args.count < 2 {
+			print("Usage: dict <word>")
+			return
+		}
 
-        let word = args[1]
-            if let definition = define(word) {
-                print(definition)
-            } else {
-                print("No definition found for \(word)")
-            }
-    }
+		let word = args[1]
+		if let definition = define(word) {
+			print(definition)
+		} else {
+			print("No definition found for \(word)")
+		}
+	}
 }
--- jobs.swift
+++ jobs.swift
@@ -1,26 +1,53 @@
-// This is the source for bin/dict
-// Compile with `swiftc -parse-as-library -o bin/dict`
+// This is the source for bin/jobs
+// Compile with `swiftc -parse-as-library -o bin/jobs jobs.swift`
+//
+// USAGE: jobs <states> <names> <dirs>
+// ARGUMENTS:
+//   <states>	zero or more states
+//   <names>	zero or more names
+//   <dirs>		zero or more dirs
+//
+//    Each array element should be followed by "^^^". The arrays must be the
+//    same length. Each index of the various arrays refers to the job state,
+//    name, and directory for a job to be displayed. The format of each is
+//    documented in `zshmodules(1)`.
+//
+//    The intended usage is to call with the values of $jobstates, $jobtexts, and $jobdirs passed through `printf(1)` as
+//    in the first example below.
+//
+// EXAMPLE:
+//   jobs "$(printf '%s^^^' "${jobstates[@]}")" "$(printf '%s^^^' "${jobtexts[@]}")" "$(printf '%s^^^' "${jobdirs[@]}")"
+//   jobs "suspended::1234=suspended^^^" "nvim jobs.swift^^^" "/Users/caleb/.dotfiles/^^^"
 
 import Foundation
-
 
 @main
 struct Job {
 	static func main() {
 		let args = CommandLine.arguments
-			let states = args[1].split(separator: "^^^").map(String.init)
-			let names = args[2].split(separator: "^^^").map(String.init)
-			let dirs = args[3].split(separator: "^^^").map(String.init)
+		let states = array(fromArg: args[1])
+		let names = array(fromArg: args[2])
+		let dirs = array(fromArg: args[3])
 
-			let out: [String] = states.indices.compactMap { i in
-				guard states[i].hasPrefix("suspended:"), dirs[i].hasPrefix(FileManager.default.currentDirectoryPath)
-					else {
-						return nil
-					}
-					return "\(names[i].split(separator: " ").first!):\(i)"
-			}
+		var out: [String] = []
+		for i in states.indices
+		where states[i].hasPrefix("suspended:")
+			&& dirs[i].hasPrefix(FileManager.default.currentDirectoryPath)
+		{
+			out.append("\(names[i].split(separator: " ").first!):\(i+1)")
+		}
 		if !out.isEmpty {
 			print("[\(out.joined(separator: " "))]")
 		}
+	}
+
+	private static func array(fromArg arg: String) -> [String] {
+		let ary: [String] =
+			arg
+			.split(separator: "^^^", omittingEmptySubsequences: false)
+			.map(String.init)
+			.dropLast(1)
+		print(ary.debugDescription)
+		return ary
 	}
 }
